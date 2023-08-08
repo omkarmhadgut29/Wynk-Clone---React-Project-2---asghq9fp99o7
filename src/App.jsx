64fda97820eb52a82da80a5023aadce221b21f91
omkarmhadgut29 <omkarmhadgut29@gmail.com>
2023-08-08 20:11:32 +0530

    initial app setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Box, Typography } from "@mui/material";
+
+function App() {
+    return (
+        <>
+            <Box>
+                <Typography className="p-[10px] bg-[cyan] ">
+                    Wynk Clone
+                </Typography>
+            </Box>
+        </>
+    );
+}
+
+export default App;
